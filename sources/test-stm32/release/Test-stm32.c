4b0bc08a164248c4a002bba3d6ade7732bf2c002
John Hall <john@simplerobots.com>
2023-01-17 17:03:13 -0800

    Adding a timer/interrupt based output for mcu tests.

--- sources/test-stm32/release/Test-stm32.c
+++ sources/test-stm32/release/Test-stm32.c
@@ -23,6 +23,13 @@ static TestHelperInfo** g_test_helper_tail = &g_test_helper_head;
 
 static TestContext* g_test_context = NULL;
 
+static TIM_HandleTypeDef g_htim7;
+
+static volatile size_t g_buffer_head = 0;
+static volatile size_t g_buffer_tail = 0;
+static volatile char g_buffer[32];
+static volatile bool g_is_output_running = false;
+
 
 extern void AddTestCaseToList(TestCaseInfo* info)
 {
@@ -40,7 +47,32 @@ extern void AddTestHelperToList(TestHelperInfo* info)
 
 static void TestOutput(void* ignore, char c)
 {
-	ITM_SendChar(c);
+	if (__get_IPSR() != 0U)
+	{
+		// Just add this character to the buffer if there is room.
+		size_t head = g_buffer_head;
+		size_t tail = g_buffer_tail;
+		if (head - tail < sizeof(g_buffer))
+		{
+			g_buffer[head % sizeof(g_buffer)] = c;
+			g_buffer_head = head + 1;
+		}
+	}
+	else if (g_is_output_running)
+	{
+		// Wait for the timer to send everything currently in the buffer.
+		size_t head = g_buffer_head;
+		size_t tail = g_buffer_tail;
+		while (head - tail > 0)
+			tail = g_buffer_tail;
+		// Add this character to the output buffer.
+		g_buffer[head % sizeof(g_buffer)] = c;
+		g_buffer_head = head + 1;
+	}
+	else
+	{
+		ITM_SendChar(c);
+	}
 }
 
 static void TestFormat(const char* format, ...)
@@ -133,8 +165,63 @@ extern bool RunTestCase(const TestCaseInfo* info)
 	return !current_context.failed;
 }
 
+static void BackgroundTimerInit()
+{
+	__HAL_RCC_TIM7_CLK_ENABLE();
+
+	g_htim7.Instance = TIM7;
+	g_htim7.Init.Prescaler = 0;
+	g_htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
+	g_htim7.Init.Period = 900;
+	g_htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+	if (HAL_TIM_Base_Init(&g_htim7) != HAL_OK)
+		Error_Handler();
+
+	TIM_MasterConfigTypeDef master_config = {0};
+	master_config.MasterOutputTrigger = TIM_TRGO_RESET;
+	master_config.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+	if (HAL_TIMEx_MasterConfigSynchronization(&g_htim7, &master_config) != HAL_OK)
+		Error_Handler();
+
+	HAL_NVIC_SetPriority(TIM7_IRQn, 5, 0);
+	HAL_NVIC_EnableIRQ(TIM7_IRQn);
+	if (HAL_TIM_Base_Start_IT(&g_htim7) != HAL_OK)
+		Error_Handler();
+
+	g_is_output_running = true;
+}
+
+static void BackgroundTimerDeInit()
+{
+	g_is_output_running = false;
+
+	if (HAL_TIM_Base_DeInit(&g_htim7) != HAL_OK)
+		Error_Handler();
+
+	__HAL_RCC_TIM7_CLK_DISABLE();
+}
+
+static void BackgroundTimerFlush()
+{
+	while (g_buffer_head != g_buffer_tail)
+		;
+}
+
+extern void TIM7_IRQHandler()
+{
+	__HAL_TIM_CLEAR_IT(&g_htim7, TIM_IT_UPDATE);
+	size_t head = g_buffer_head;
+	size_t tail = g_buffer_tail;
+	if (tail < head && ITM->PORT[0U].u32 != 0)
+	{
+		ITM->PORT[0U].u8 = g_buffer[tail % sizeof(g_buffer)];
+		g_buffer_tail = tail + 1;
+	}
+}
+
 extern void RLM3_Main_CB()
 {
+	BackgroundTimerInit();
 	TestFormat("== RUNNING TEST CASES ==\n");
 
 	size_t total_test_count = 0;
@@ -153,6 +240,8 @@ extern void RLM3_Main_CB()
 		{
 			TestFormat("=== TEST FAILED: %s File '%s' line %d ===\n", cursor->name, cursor->file, cursor->line);
 		}
+
+		BackgroundTimerFlush();
 	}
 
 	TestFormat("== TEST SUMMARY ==\n");
@@ -169,5 +258,8 @@ extern void RLM3_Main_CB()
 		TestFormat("== TESTS FAILED ==\n");
 		TestFormat("EOT FAIL\n");
 	}
+
+	BackgroundTimerFlush();
+	BackgroundTimerDeInit();
 }
 
