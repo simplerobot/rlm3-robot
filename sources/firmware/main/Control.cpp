03f0923d424f62cc62ebdc13d07b9476f566cb51
John Hall <john@simplerobots.com>
2023-01-18 16:06:28 -0800

    Fixed build order. Fixed control task. Added tests for control task. Added sim tasks.

--- sources/firmware/main/Control.cpp
+++ sources/firmware/main/Control.cpp
@@ -1,6 +1,7 @@
 #include "Control.hpp"
 #include "rlm3-task.h"
 #include "rlm3-motors.h"
+#include "rlm3-lock.h"
 
 
 constexpr RLM3_Time MAX_CONTROL_DELAY_MS = 100;
@@ -17,22 +18,37 @@ static volatile float g_control_blade = 0;
 extern void Control_RunTask()
 {
 	bool is_active = false;
+	g_task = RLM3_Task_GetCurrent();
 
 	RLM3_Motors_Init();
 
 	while (!g_abort)
 	{
-		if (RLM3_Time_Get() - g_control_time <= MAX_CONTROL_DELAY_MS)
+		uint32_t current_time = RLM3_Time_Get();
+
+		RLM3_Lock_EnterCritical();
+		uint32_t control_time = g_control_time;
+		float control_left = g_control_left;
+		float control_right = g_control_right;
+		float control_blade = g_control_blade;
+		RLM3_Lock_ExitCritical();
+
+		if (current_time - control_time < MAX_CONTROL_DELAY_MS)
 		{
 			// Make sure motors are active.
 			if (!is_active)
 			{
 				RLM3_Motors_Enable();
+				is_active = true;
 			}
 
 			// Move.
-			RLM3_Motors_SetWheels(g_control_left, g_control_right);
-			RLM3_Motors_SetBlade(g_control_blade);
+			RLM3_Motors_SetWheels(control_left, control_right);
+			RLM3_Motors_SetBlade(control_blade);
+
+			// Wait up to the cutoff time for another command.
+			RLM3_Time delay = MAX_CONTROL_DELAY_MS - (current_time - control_time);
+			RLM3_Task_TakeUntil(current_time, delay);
 		}
 		else
 		{
@@ -43,16 +59,27 @@ extern void Control_RunTask()
 				is_active = false;
 			}
 
-			// Make sure motors will not move.
-			g_control_left = 0.0f;
-			g_control_right = 0.0f;
-			g_control_blade = 0.0f;
-		}
+			// Make doubly sure that that the motors stay disabled.
+			if (control_left != 0 || control_right != 0 || control_blade != 0)
+			{
+				RLM3_Lock_EnterCritical();
+				if (control_time != g_control_time)
+				{
+					g_control_left = 0;
+					g_control_right = 0;
+					g_control_blade = 0;
+				}
+				RLM3_Lock_ExitCritical();
+			}
 
-		RLM3_Task_TakeWithTimeout(50);
+			// Wait for a new command to come in.
+			RLM3_Task_Take();
+		}
 	}
 	g_abort = false;
 
+	if (is_active)
+		RLM3_Motors_Disable();
 	RLM3_Motors_DeInit();
 }
 
@@ -70,10 +97,12 @@ extern void Control_AbortTaskISR()
 
 extern void CommInput_Control_Callback(uint32_t time, float left, float right, float blade)
 {
+	RLM3_Lock_EnterCritical();
 	g_control_left = left;
 	g_control_right = right;
 	g_control_blade = blade;
 	g_control_time = time;
+	RLM3_Lock_ExitCritical();
 
 	RLM3_Task_Give(g_task);
 }
--- sources/firmware/test-cpu/Control-Tests.cpp
+++ sources/firmware/test-cpu/Control-Tests.cpp
@@ -1,4 +1,82 @@
 #include "Test.hpp"
 #include "Control.hpp"
+#include "rlm3-sim.hpp"
+#include "rlm3-motors.h"
+#include "CommInput.hpp"
+#include "logger.h"
 
+
+LOGGER_ZONE(TEST);
+
+
+TEST_CASE(Control_Abort)
+{
+	SIM_AddInterrupt([] { Control_AbortTaskISR(); });
+
+	Control_RunTask();
+}
+
+TEST_CASE(Control_AbortRestart)
+{
+	SIM_AddInterrupt([] { Control_AbortTaskISR(); });
+	SIM_AddInterrupt([] { Control_AbortTaskISR(); });
+
+	Control_RunTask();
+	Control_RunTask();
+}
+
+static void ValidateMotor(const char* name, float expected, float actual)
+{
+	if (expected == 0.0f && actual != 0.0f)
+		FAIL("Motor %s expected %f actual %f diff %f", name, expected, actual, expected - actual);
+	if (std::abs(expected - actual) > 0.01f)
+		FAIL("Motor %s expected %f actual %f diff %f", name, expected, actual, expected - actual);
+}
+
+static void ValidateMotors(float expected_left, float expected_right, float expected_blade)
+{
+	ValidateMotor("left", expected_left, SIM_Motors_GetLeft());
+	ValidateMotor("right", expected_right, SIM_Motors_GetRight());
+	ValidateMotor("blade", expected_blade, SIM_Motors_GetBlade());
+}
+
+TEST_CASE(Control_HappyCase)
+{
+	SIM_AddTask([] { ValidateMotors(0.0, 0.0, 0.0); });
+	SIM_AddDelay(200);
+	SIM_AddTask([] { CommInput_Control_Callback(RLM3_Time_Get(), 0.5, 0.3, 0.8); });
+	SIM_AddTask([] { ValidateMotors(0.5, 0.3, 0.8); });
+	SIM_AddDelay(60);
+	SIM_AddTask([] { ValidateMotors(0.5, 0.3, 0.8); });
+	SIM_AddTask([] { CommInput_Control_Callback(RLM3_Time_Get(), -0.2, -0.3, -0.5); });
+	SIM_AddTask([] { ValidateMotors(-0.2, -0.3, -0.5); });
+	SIM_AddInterrupt([] { Control_AbortTaskISR(); });
+
+	Control_RunTask();
+}
+
+TEST_CASE(Control_LostSignal)
+{
+	SIM_AddTask([] { ValidateMotors(0.0, 0.0, 0.0); });
+	SIM_AddDelay(200);
+	SIM_AddTask([] { CommInput_Control_Callback(RLM3_Time_Get(), 0.5, 0.3, 0.8); });
+	SIM_AddTask([] { ValidateMotors(0.5, 0.3, 0.8); });
+	SIM_AddDelay(100);
+	SIM_AddTask([] { ValidateMotors(0.5, 0.3, 0.8); });
+	SIM_AddDelay(1);
+	SIM_AddTask([] { ValidateMotors(0.0, 0.0, 0.0); });
+	SIM_AddInterrupt([] { Control_AbortTaskISR(); });
+
+	Control_RunTask();
+}
+
+TEST_CASE(Control_LateSignal)
+{
+	SIM_AddDelay(200);
+	SIM_AddTask([] { CommInput_Control_Callback(RLM3_Time_Get() - 100, 0.5, 0.3, 0.8); });
+	SIM_AddTask([] { ValidateMotors(0.0, 0.0, 0.0); });
+	SIM_AddInterrupt([] { Control_AbortTaskISR(); });
+
+	Control_RunTask();
+}
 
